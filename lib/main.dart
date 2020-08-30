62f55aaeed399276890d893a90fda40a4377b97c
wadthyk <66349360+wadthyk@users.noreply.github.com>
2020-08-30 16:25:03 +0900

    design_change_04

--- lib/main.dart
+++ lib/main.dart
@@ -110,7 +110,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: Icon(Icons.money_off),
+        child: Icon(Icons.category),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
